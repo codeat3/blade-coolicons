0e16709e936fb69e41c4f4f8eee7d1e842865546
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-30 16:42:00 +0530

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+This changelog follows [the Keep a Changelog standard](https://keepachangelog.com).
+
+
+## 1.0.0 (2021-05-30)
+Initial release.
+Synced with the latest release [Coolicons - v2.5](https://github.com/krystonschwarze/coolicons/releases/tag/v2.5)
---
+++ src/BladeCooliconsServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeat3\BladeCoolicons;
+
+use BladeUI\Icons\Factory;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
+
+final class BladeCooliconsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-coolicons', []);
+
+            $factory->add('coolicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+        });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-coolicons.php', 'blade-coolicons');
+    }
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-coolicons'),
+            ], 'blade-coolicons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-coolicons.php' => $this->app->configPath('blade-coolicons.php'),
+            ], 'blade-coolicons-config');
+        }
+    }
+
+}
